d3df8c21ff012352552bc874363bc6e886fcccfb
Robert Michalski <majointa@gmail.com>
2026-09-20 22:04:33 +0200

    fix pdf

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
-export default withNextIntl({ poweredByHeader: false });
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  serverExternalPackages: ["@sparticuz/chromium"],
+};
+
+export default withNextIntl(nextConfig);
