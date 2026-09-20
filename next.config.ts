b30c30e6f91764d271b57d99ef9c2efc5ed7e6c9
Robert <67601105+michalskirobert@users.noreply.github.com>
2026-09-20 22:10:42 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   serverExternalPackages: ["@sparticuz/chromium"],
+outputFileTracingIncludes: {
+    "/api/documents/[id]/pdf": [
+      "./node_modules/@sparticuz/chromium/bin/**",
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
